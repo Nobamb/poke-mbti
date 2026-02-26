e13c4c49134b9932c4d149c3c697da0940644109
nobamb <jsh12345644@gmail.com>
2026-02-26 22:56:17 +0900

    mbti, pokemon 가져옴, mbti와 pokemon에서 value값과 일치하는 값 추출

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// mbti 가져옴
+import MBTI from "./mbti.js";
+// pokemon 가져옴
+import pokemon from "./pokemon.js";
+
 // 버튼 요소를 받음
 const formButton = document.querySelector("form>button");
 
@@ -17,11 +22,46 @@ formButton.addEventListener("click", (e) => {
   const inputValue = document.querySelector("form>input").value;
 
   // data의 값을 보정(무조건 대문자로 보정)
-  const getMBTI = inputValue.toUpperCase()
+  const getMBTIValue = inputValue.toUpperCase();
 
   // mbti 출력 테스트
   // console.log(getMBTI)
 
+  // 포켓몬과 mbti 변수 초기화
+  let getPokemon;
+  let getMBTIData;
+
+  // getMBTIValue를 가져와서 mbti와 pokemon의 mbti값 비교
+
+  // pokemon과 비교
+  pokemon.forEach((element) => {
+    // 만약에 element의 mbti와
+    // getMBTIValue가 동일하다면
+    if (element.mbti === getMBTIValue) {
+    
+      // getPokemon을 저장
+      getPokemon = element
+    
+    }
+  });
+
+  // mbti와 비교
+  MBTI.forEach((element) => {
+    // 만약에 element의 mbti와
+    // getMBTIValue가 동일하다면
+    if (element.mbti === getMBTIValue) {
+    
+      // getPokemon을 저장
+      getMBTIData = element
+    
+    }
+  });
+
+  // 결과 출력 테스트
+  // 포켓몬(이름 출력하게 해봄)
+  console.log(getPokemon.name)
+  // MBTI(설명 출력하게 해봄)
+  console.log(getMBTIData.description)
 
   // fetch 진행
   fetch(`https://pokeapi.co/api/v2/pokemon-species/${inputValue}/`, {
