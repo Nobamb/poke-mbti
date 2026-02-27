a30b5c2c0081d2721cf8372889c50afd25a45712
nobamb <jsh12345644@gmail.com>
2026-02-28 01:19:27 +0900

    getResultAPI를 main에서 제거, question에 넣도록 함

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-// getResultAPI받음
-import getResultAPI from "./func/getResultAPI.js";
-
 // quesition 받음
 import question from "./question.js";
-
-// 포켓몬 api를 받는 함수
-getResultAPI()
 
 // qustion 실행
 // mbti 관련 질문들을 하도록 함
